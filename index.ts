f4f332c93cb314f91d65afbd662c3e75f04439cc
rohit <nsrohit16@gmail.com>
2022-06-22 00:44:33 +0530

    update config files

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
-import { UserBuilder } from "./builder";
+/* import { UserBuilder } from "./builder/example1";
 
 const user = new UserBuilder("rohit").setAge(12).setPhone(12).setAddress({
   city: "pune",
   zip: 12345,
 });
+
+console.log(user); */
+
+import { PersonBuilder } from "./builder";
+
+const pb = new PersonBuilder();
+const user = pb
+  .setName("Ankit")
+  .setAge(27)
+  .setAddress()
+  .setHomeAddress({
+    city: "banglore",
+    pincode: 123456,
+    streeName: "harlur",
+  })
+  .setEmploymentDetails()
+  .earning(5000000)
+  .designation("Senior Software Engineer")
+  .worksAt("Amazon");
 
 console.log(user);
